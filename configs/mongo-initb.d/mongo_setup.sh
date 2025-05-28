2cbd0b20c5f1f1e2ebedba13732543a47ef16d81
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2025-05-28 14:05:54 +0200

    Refer to mongo-dev in setup script and container name set to mongo-dev

--- configs/mongo-initb.d/mongo_setup.sh
+++ configs/mongo-initb.d/mongo_setup.sh
@@ -7,14 +7,14 @@ echo "📅 mongo_setup.sh time now: $(date +"%T")"
 
 echo "⚙️ Initiating Replica Set..."
 
-mongosh --host mongo:27017 <<EOF
+mongosh --host mongo-dev:27017 <<EOF
 rs.initiate({
   _id: "rs0",
   version: 1,
   members: [
     {
       _id: 0,
-      host: "mongo:27017",
+      host: "mongo-dev:27017",
       priority: 1
     }
   ]

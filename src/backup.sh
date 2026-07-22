54aa18d8216d5cc189b8d33cd5297c0f74aeb968
Rodolfo Marraffa <rodolfo.marraffa@gbv.de>
2026-07-22 17:16:54 +0200

    Disable automatic Git maintenance during backups #76

--- src/backup.sh
+++ src/backup.sh
@@ -3,11 +3,35 @@
 repo=/home/cocoda/backup
 repo_mounted=/backup
 
+# Run Git without automatic garbage collection or maintenance
+git_repo() {
+  git \
+    -C "$repo" \
+    -c gc.auto=0 \
+    -c maintenance.auto=false \
+    "$@"
+}
+
 # TODO: Add more databases/collections if necessary
 # shellcheck disable=SC2016
-srv exec mongo -iT mongo mongodump --quiet --db jskos-server --collection mappings --query '{ "uri": { "$exists": true } }' --out $repo_mounted
-srv exec mongo -iT mongo mongodump --quiet --db jskos-server --collection concordances --out $repo_mounted
-srv exec mongo -iT mongo mongodump --quiet --db jskos-server --collection annotations --out $repo_mounted
+srv exec mongo -iT mongo \
+  mongodump --quiet \
+  --db jskos-server \
+  --collection mappings \
+  --query '{ "uri": { "$exists": true } }' \
+  --out "$repo_mounted"
+
+srv exec mongo -iT mongo \
+  mongodump --quiet \
+  --db jskos-server \
+  --collection concordances \
+  --out "$repo_mounted"
+
+srv exec mongo -iT mongo \
+  mongodump --quiet \
+  --db jskos-server \
+  --collection annotations \
+  --out "$repo_mounted"
 
 # convert bson to json
 for file in "$repo"/*/*.bson
@@ -15,10 +39,14 @@ do
   file_json=${file%*bson}ndjson
   bsondump --quiet --outFile "$file_json" "$file"
 done
-# add and commit
-git -C $repo/ add .
-if [[ $(git -C $repo/ diff --cached --name-only) ]]
+
+# Add and commit
+git_repo add .
+
+if ! git_repo diff --cached --quiet
 then
   date_adjusted=$(date --iso-8601=seconds)
-  git -C $repo/ commit --date "$date_adjusted" -m "$date_adjusted"
+  git_repo commit \
+    --date "$date_adjusted" \
+    -m "$date_adjusted"
 fi
